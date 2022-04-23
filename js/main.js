6bbd827c742636044dc276775b0ed79e95e6d163
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-04-23 14:16:22 +0900

    ディレクトリ整理

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
-title.load();
+const backImgArr = ["サンシャイン栄.png", "ローマ.png", "名古屋城.png"];
+backImg(backImgArr);
 
-// モーダルのテスト
-
-// const modalNode = new MODAL(
-//   "main",
-//   document.getElementById("main"),
-//   "Let's talk",
-//   "Hello",
-//   "Oooopen"
-// );
-
-// /* 共通する処理 */
-// let state = 1;
-
-// switch (state) {
-//   case 1:
-//     /* ロード画面 - 開始 */
-//     title.load();
-//     break;
-//   case 2:
-//     /* ポートフォリオ画面 - 開始*/
-//     portfolio ? "a" : "b";
-//     break;
-// }
+load.start(8000);
--- リファクタリング/js/main.js
+++
@@ -1,4 +0,0 @@
-const backImgArr = ["サンシャイン栄.png", "ローマ.png", "名古屋城.png"];
-backImg(backImgArr);
-
-load.start(8000);
